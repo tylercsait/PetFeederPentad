b7a714e64d938025780e6aac36907066cc59b516
t.chow <tyler.h.chow@gmail.com>
2024-11-01 17:40:42 -0600

    added packages in main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 """
 Pet Feeder Main File
 Collaborators: Tsz Hei Chan, Ronghao Liu, Tyler Chow, Hung-Ju Ke, Nabil Mekhalfa
-Last Modified: 10/1/2024
+Last Modified: 11/1/2024
+"""
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+"""
+This code will read the 
 """
