cf656f414989b17987778a8f8210c4a9f1633b78
t.chow <tyler.h.chow@gmail.com>
2024-11-01 19:01:04 -0600

    added reader code to main

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ Collaborators: Tsz Hei Chan, Ronghao Liu, Tyler Chow, Hung-Ju Ke, Nabil Mekhalfa
 Last Modified: 11/1/2024
 """
 import RPi.GPIO as GPIO
+import pets
 from mfrc522 import SimpleMFRC522
 
 """
-This code will read the 
+This section reads the RFID sensor and dispenses food if it is allowed
 """
+#
+
+reader = SimpleMFRC522()
+try:
+    print("Waiting for RFID")
+    uid, text = reader.read()
+    print(f"uid: '{uid}' Text: '{text}'")
+    # check to see if exists in pets
+    # if it exists in pets, do some more stuff
+    # if it does not exist, print error stuff
+finally:
+    GPIO.cleanup()
+
