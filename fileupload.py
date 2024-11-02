8f6ce77123ddb1013cf67063710b66688446ea85
t.chow <tyler.h.chow@gmail.com>
2024-11-02 13:12:03 -0600

    renamed variable for readability

--- fileupload.py
+++ fileupload.py
@@ -11,19 +11,18 @@ def get_connection_str(file_path):
 connection_string = get_connection_str("connection.txt")
 container_name = "pentadcontainer"
 
-# Create the BlobServiceClient object
 
 # Create the BlobServiceClient object
 blob_service_client = BlobServiceClient.from_connection_string(connection_string)
 container_client = blob_service_client.get_container_client(container_name)
 
-def upload_blob(file_path, blob_name):
-    blob_client = container_client.get_blob_client(blob_name)
+def upload_blob(file_path, file_in_blob_name):
+    blob_client = container_client.get_blob_client(file_in_blob_name)
 
     with open(file_path, "rb") as data:
         blob_client.upload_blob(data, overwrite=True)
 
-    print(f"{blob_name} uploaded successfully.")
+    print(f"{file_in_blob_name} uploaded successfully.")
 
 # Example usage
-upload_blob("/home/group3/test.txt", "test.txt")
+# upload_blob("/home/group3/test.txt", "test.txt")
