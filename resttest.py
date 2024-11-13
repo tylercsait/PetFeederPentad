6cefa0a233ebe56aa40f171cfce4923e29fdf79a
Tyler Chow <tyler.chow@edu.sait.ca>
2024-11-13 16:36:03 -0700

    Can dispense food

--- resttest.py
+++ resttest.py
@@ -1,7 +1,10 @@
 import requests
 
 # Home Assistant API URL
-url = "http://137.186.88.102:53218/api/"
+#url = "http://137.186.88.102:53218/api/services/number/set_value"
+url = "http://192.168.1.200:8123/api/services/number/set_value"
+
+entity_id = "number.chong_wu_wei_shi_qi_sdk_feed"
 
 def get_connection_str(file_path):
     with open(file_path, 'r') as file:
@@ -9,18 +12,22 @@ def get_connection_str(file_path):
 
 token = get_connection_str("token.txt")
 
-# API token
-
+portion_size = 1
 # Headers
 headers = {
     "Authorization": f"Bearer {token}",
     "Content-Type": "application/json"
 }
 
+data = {
+    "entity_id": f"{entity_id}",
+    "value": portion_size
+}
+
 try:
     # Send a GET request to the API
-    response = requests.get(url, headers=headers)
-
+    #response = requests.get(url, headers=headers)
+    response = requests.post(url, headers=headers, json=data)
     # Check the response
     if response.status_code == 200:
         print("Connectivity test successful! Home Assistant is accessible.")
@@ -29,3 +36,9 @@ try:
 
 except requests.exceptions.RequestException as e:
     print(f"Connection error: {e}")
+
+print(f"URL: {url}")
+print(f"Headers: {headers}")
+print(f"Data: {data}")
+print(f"Response Status Code: {response.status_code}")
+print(f"Response Text: {response.text}")
