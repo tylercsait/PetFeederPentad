6bc35903dfc0c5566126e3cf7717795a4e48791c
t.chow <tyler.h.chow@gmail.com>
2024-11-18 21:33:20 -0700

    test

--- tabletest.py
+++ tabletest.py
@@ -14,18 +14,35 @@ from datetime import datetime
 
 import registerpets
 
-def test_registerpets_process_pet_input():
-    rfid = "TEST"
-    rfid_text = "ahaha"
-    with mysql_connection() as cursor:
+def test_registerpets_process_pet_input(cursor, rfid, rfid_text):
+    #with mysql_connection() as cursor:
         # db_utils.create_pets_table(cursor)
-        db_utils.view_table(cursor, "pets")
-        registerpets.process_pet_input(cursor, rfid, rfid_text)
-        db_utils.view_table(cursor, "pets")
+    db_utils.view_table(cursor, "pets")
+    registerpets.process_pet_input(cursor, rfid, rfid_text)
+
+
+def simulate_dispense_food(cursor, rfid):#Dispense food if history is empty
+    #create_history_table(cursor)
+    if eligible_to_feed(cursor, rfid):
+        print("Pet is eligible. Feeding")
+        increment_feeding_history(cursor, rfid)
+    else:
+        print("Pet not is eligible. Not feeding")
+
 
 if __name__ == "__main__":
-    test_registerpets_process_pet_input()
-    # with mysql_connection() as db_cursor:
+    rfid = "1223334444"
+    rfid_text = "Jason"
+    with mysql_connection() as db_cursor:
+        # initialize_tables(db_cursor)
+        # test_registerpets_process_pet_input(db_cursor, rfid, rfid_text)
+        #simulate_dispense_food(db_cursor, rfid)
+        # add_pet(db_cursor, rfid, rfid_text, 3, 6,2)
+        db_utils.view_table(db_cursor, "pets")
+        db_utils.view_table(db_cursor, "history")
+        simulate_dispense_food(db_cursor, rfid)
+        db_utils.view_table(db_cursor, "pets")
+        db_utils.view_table(db_cursor, "history")
         # create_pets_table(db_cursor)
         # view_table(db_cursor,"pets")
         # add_pet(db_cursor, "TEST", 0, 5, 3, 2)
