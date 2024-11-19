469fc262e832fc51c3fe8d57cf70673be5a147b5
Tyler Chow <tyler.chow@edu.sait.ca>
2024-11-19 15:37:55 -0700

    cleaned up

--- tabletest.py
+++ tabletest.py
@@ -5,13 +5,14 @@ from tabnanny import check
 from venv import create
 
 import db_utils
+import rest_api_utils
 #from venv import create
 
 #import pets_db_utils
 #from datetime import date, datetime
 from datetime import datetime
 
-import registerpets
+# import registerpets
 
 def test_registerpets_process_pet_input(cursor, rfid, rfid_text):
     #with mysql_connection() as cursor:
@@ -25,6 +26,8 @@ def simulate_dispense_food(cursor, rfid):#Dispense food if history is empty
     if db_utils.eligible_to_feed(cursor, rfid):
         print("Pet is eligible. Feeding")
         db_utils.increment_feeding_history(cursor, rfid)
+        rest_api_utils.dispense_portions(1)
+
     else:
         print("Pet not is eligible. Not feeding")
 
@@ -33,33 +36,11 @@ if __name__ == "__main__":
     rfid = "1223334444"
     rfid_text = "Jason"
     with db_utils.mysql_connection() as db_cursor:
-        # initialize_tables(db_cursor)
-        # test_registerpets_process_pet_input(db_cursor, rfid, rfid_text)
-        #simulate_dispense_food(db_cursor, rfid)
-        # add_pet(db_cursor, rfid, rfid_text, 3, 6,2)
-        db_utils.view_table(db_cursor, "pets")
-        db_utils.view_table(db_cursor, "history")
         # simulate_dispense_food(db_cursor, rfid)
         db_utils.view_table(db_cursor, "pets")
         db_utils.view_table(db_cursor, "history")
-        # create_pets_table(db_cursor)
-        # view_table(db_cursor,"pets")
-        # add_pet(db_cursor, "TEST", 0, 5, 3, 2)
-        # view_table(db_cursor,"pets")
-        # create_history_table(db_cursor)
-        # add_history(db_cursor,"HISTORY", datetime.now().time(), 2)
-        # view_table(db_cursor, "history")
-        # print(check_pet_exists(db_cursor,"HISTORY"))
+        db_utils.view_table(db_cursor, "pets")
+        db_utils.view_table(db_cursor, "history")
 
-#todaydate = date.today().isoformat()
-#pets.delete_all_pets()
-# Example usage
-#pets.add_pet("69", 1, 1)
-#pets.add_pet("12", 2, 1)
-#pets.update_pet_feedings_today("69", todaydate)
-#pets.update_pet_portion_size('69', 5)
-#print(pets.check_pet_exists("12"))
-#delete_pet("69", todaydate)
-#pets.list_all_pets()
-#pets.delete_all_pets()
+
 
