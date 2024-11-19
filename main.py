740094e272b2857e7aae81c684f0a55bcf8ebae8
Tyler Chow <tyler.chow@edu.sait.ca>
2024-11-19 15:35:37 -0700

    logic for 2 states

--- main.py
+++ main.py
@@ -13,21 +13,29 @@ import time
 
 GRAMS_PER_PORTION = 8 # 7-9 grams per portion, 8 is the middle
 
+def handle_rfid(cursor, rfid):
+    if db_utils.eligible_to_feed(cursor, rfid):
+        print("Pet is eligible. Feeding")
+        num_portions = db_utils.get_portion_per_feeding(cursor, rfid)
+        rest_api_utils.dispense_portions(num_portions)
+        db_utils.increment_feeding_history(cursor, rfid)
+
+    else:
+        print("Pet is not eligible. Not feeding")
+
 if __name__ == "__main__":
     with db_utils.mysql_connection() as db_cursor:
         print("This program dispenses food if an RFID tag is detected and the tag is in our database")
         try:
+            occupied = False
             while True:
                 rfid, rfid_text = rfid_utils.read_rfid()
                 # Check if eligible to feed
-                if db_utils.eligible_to_feed(db_cursor, rfid):
-                    print("Pet is eligible. Feeding")
-                    num_portions = db_utils.get_portion_per_feeding(db_cursor, rfid)
-                    rest_api_utils.dispense_portions(num_portions)
-                    db_utils.increment_feeding_history(db_cursor, rfid)
-
-                else:
-                    print("Pet is not eligible. Not feeding")
+                if not occupied:
+                    handle_rfid(db_cursor, rfid)
+                    occupied = True
+                elif occupied:
+                    occupied = False
 
                 # Optional: Add a small delay to avoid rapid looping
                 time.sleep(1)
