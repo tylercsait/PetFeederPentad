de082848f3cfc1bb277d78d3d2c60f4662e850c5
t.chow <tyler.h.chow@gmail.com>
2024-11-22 14:39:21 -0700

    Added view pets page

--- app.py
+++ app.py
@@ -4,15 +4,24 @@ from registerpets import process_pet_input
 
 app = Flask(__name__)
 
-# Route for the SmartFeeder page
+# Route for the register pets page
 @app.route('/register')
 def register():
     return render_template('register.html')
 
-# Default route for the directory page (now index.html)
+# Default route for the directory page (index.html)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Route for viewing all registered pets
+@app.route('/view-pets')
+def view_pets():
+    with db_utils.mysql_connection() as cursor:
+        cursor.execute("SELECT rfid, rfid_text, max_feedings_day, max_portions_day, portions_per_feeding FROM pets")
+        pets = cursor.fetchall()
+        pets = [dict(rfid=row[0], rfid_text=row[1], max_feedings_day=row[2], max_portions_day=row[3], portions_per_feeding=row[4]) for row in pets]
+    return render_template('view_pets.html', pets=pets)
 
 # Route for another page
 @app.route('/another-page')
