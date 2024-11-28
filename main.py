e63f32565c05847cc6a29890a1d04af171957586
Tyler Chow <tyler.chow@edu.sait.ca>
2024-11-28 16:32:03 -0700

    Uncommented camera code

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ def handle_rfid_occupied(cursor, rfid, previous_leftover_portions):
         filename = db_utils.create_file_name(cursor, rfid)
         location = 'home/group3/'
         # Then take a picture and upload it
-        # camera.capture_image(location, filename)
-        # fileupload.upload_jpg_blob(f"{location}{filename}", filename)
+        camera.capture_image(location, filename)
+        fileupload.upload_jpg_blob(f"{location}{filename}", filename)
 
     else:
         print("Pet is not eligible. Not feeding")
