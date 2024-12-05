6e5d46e651e4456e1734f0ae776d17ee6d586cc9
Tyler Chow <tyler.chow@edu.sait.ca>
2024-12-05 14:33:23 -0700

    surrounded code with try finally. closed camera

--- camera.py
+++ camera.py
@@ -1,13 +1,17 @@
 from picamera2 import Picamera2
 
 def capture_image(location, filename):
-    picam2 = Picamera2()
-    config = picam2.create_still_configuration()
-    picam2.configure(config)
-    picam2.start()
-    picam2.capture_file(f'{location}{filename}')
-    picam2.stop()
-    print(f"Image captured and saved as '{location}{filename}'")
+    try:
+        picam2 = Picamera2()
+        config = picam2.create_still_configuration()
+        picam2.configure(config)
+        picam2.start()
+        picam2.capture_file(f'{location}{filename}')
+        picam2.stop()
+        print(f"Image captured and saved as '{location}{filename}'")
+    finally:
+        picam2.close()
+
 
 if __name__ == "__main__":
     capture_image()
